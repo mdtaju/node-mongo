6452444c396ef21e51536c0ffe765672ef9a58b4
mdtaju <tajumd512ctg@gmail.com>
2020-08-21 09:41:42 +0600

    Initial commit

---
+++ index.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const MongoClient = require('mongodb').MongoClient;
+
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+const name = "newDbUser";
+const pass = "p601H4CciHL6KkEf";
+const uri = "mongodb+srv://newDbUser:p601H4CciHL6KkEf@cluster0.xxpdh.mongodb.net/newDbUser?retryWrites=true&w=majority";
+let client = new MongoClient(uri, { useNewUrlParser: true });
+
+app.get('/getAppointment', (req, res) =>{
+    client = new MongoClient(uri, { useNewUrlParser: true });
+    client.connect(err => {
+        const collection = client.db("newOne").collection("appointment");
+        collection.find().toArray((err, documents)=>{
+            if(err){
+                console.log(err)
+                res.status(500).send({message:err});
+            }
+            else{
+                res.send(documents);
+            }
+        });
+        client.close();
+    });
+});
+
+app.post('/appointment', (req, res) => {
+    const appointment = req.body;
+    client = new MongoClient(uri, { useNewUrlParser: true });
+    client.connect(err => {
+        const collection = client.db("newOne").collection("appointment");
+        collection.insertOne(appointment, (err, result)=>{
+            if(err){
+                console.log(err)
+                res.status(500).send({message:err});
+            }
+            else{
+                res.send(result.ops[0]);
+            }
+        });
+        client.close();
+    });
+});
+
+app.listen(4200, () => console.log('Listenting to port 4200'));
